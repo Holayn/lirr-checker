e4c9ebcb0fe66772bbcd87ea760071f38cff609d
Holayn <kwwong1597@gmail.com>
2026-02-19 12:32:31 -0500

    Remove unnecessary message about there being no real-time info.
    Assume that no real-time info = no delay.

--- index.js
+++ index.js
@@ -90,10 +90,9 @@ async function checkDeparture(entry, gtfsData) {
   for (const trip of trips) {
     const delayInfo = getTripDelay(feed, trip.tripId, trip.srcStopId);
     const status = formatDelay(delayInfo.delay);
-    const rtNote = delayInfo.found ? '' : ', (no real-time delay found)';
     const msg =
       `Train from ${trip.srcStopName} to ${trip.dstStopName}, ` +
-      `departing ${trip.scheduledDep}, is ${status}${rtNote}.`;
+      `departing ${trip.scheduledDep}, is ${status}.`;
 
     announcements.push({ msg, audio });
     postNotification(msg, users)
